fc25ea9839c2376bf4d48bd3a664d5edc1cdb39d
Pondstales <132643257+Pondstales@users.noreply.github.com>
2023-06-12 13:58:19 +0700

    Create pizza_chatbot.py

---
+++ 07_Python/pizza_chatbot.py
@@ -0,0 +1,13 @@
+def chatbot():
+    pizzas = []
+    print("Instruction: Type 'exit' to end the program")
+    print("""Welcome to our pizza restaurant! Here's the menu
+Pizza Menu: Pepperoni, Margherita, Hawaiian, Cheese""")
+    
+    while True:
+        pizza = input("What pizza do you want to order? ")
+        if pizza == "exit":
+            print(f"Thank you! Your orders are ({len(pizzas)}):")
+            return pizzas
+        pizzas.append(pizza)
+        
